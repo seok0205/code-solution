4e6ea59f2a9c34a44c33038a6a2d9cd5b8f3b09f
seok0205 <9526yu@naver.com>
2025-02-22 02:43:38 +0900

    update 250221 D3 1289 원재의 메모리 복구하기

--- sw_expert_academy/25.02/21_D3_1289.py
+++ sw_expert_academy/25.02/21_D3_1289.py
@@ -6,18 +6,18 @@ D3 1289 원재의 메모리 복구하기
 원래 상태가 주어질때 초기화 상태에서 원래 상태로 돌아가는데 최소 몇번을 고쳐야하는 지 계산
 '''
 
-import sys
-sys.stdin = open('tc.txt', 'r')
-
 T = int(input())
 
 for tc in range(1, T+1):
     target = list(map(int, input()))
     bit = [0] * len(target)
+    result = 0
     
-    for i in range(len(bit)):
-        if bit[i] != target[i]:
-            for j in range(i, len(bit)):
-                bit[i] = int(not bit[i])
-
-    print(bit)
+    for i in range(len(bit)):       # 비트 길이(탐색해야할 리스트) 만큼 반복
+        if bit[i] != target[i]:     # 초기화된 상태의 자리와 목표 리스트의 자리가 다르면
+            result += 1             # 바꾼 횟수 증가
+            num = bit[i] ^ 1        # 0 or 1 변환
+            for j in range(i, len(bit)):        # 뒷자리 모두
+                bit[j] = num                    # 같은 숫자로 설정
+    
+    print(f'#{tc} {result}')
