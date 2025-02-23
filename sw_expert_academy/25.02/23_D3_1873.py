8a7ec1dca7357aa4dd9eda02e781918a7d46a2a6
seok0205 <9526yu@naver.com>
2025-02-23 13:40:57 +0900

    update 250223 D3 1873 상호의 배틀필드

--- sw_expert_academy/25.02/23_D3_1873.py
+++ sw_expert_academy/25.02/23_D3_1873.py
@@ -30,57 +30,73 @@ S	Shoot : 전차가 현재 바라보고 있는 방향으로 포탄을 발사한�
 초기맵 상태 및 사용자 입력이 주어질 때, 최종 게임 맵의 상태를 구하는 문제
 '''
 
-import sys
-sys.stdin = open('tc.txt', 'r')
-
 
 def shoot(mobile, field):
-    move_dic = {'<': (1, 0), '>': (0, 1), '^': (-1, 0), 'v': (0,-1)}     # 이동 방향
+    '''
+    mobile : 탱크의 현재 상태 (바라보는 방향, i좌표, j좌표)
+    field : 이 함수를 실행하기 전 지도의 상태. 이 것을 포탄 발사한 뒤의 지도를 반환
+    '''
+    move_dic = {'<': (0, -1), '>': (0, 1), '^': (-1, 0), 'v': (1, 0)}     # 이동 방향 딕셔너리
     
     direction = move_dic[mobile[0]]
     idx = 0
 
     x = mobile[1]   # 전차가 현재 존재하는 위치에서 포탄 발사하므로 전차의 위치가 포탄의 출발 지점
     y = mobile[2]
-    while True:     # 델타 활용(move_dic 활용)
+    while True:     # move_dic 딕셔너리 활용. 포탄은 쭉 날라가면서 어디까지 갈지 모름. 따라서 while 사용
         idx += 1
-        mx = x + direction[0] * idx
+        mx = x + direction[0] * idx     # 한칸씩 늘려가면서 탐색
         my = y + direction[1] * idx
         if 0 <= mx < H and 0 <= my < W and field[mx][my] == "*":        # 벽돌로 지어진 벽 만나면 파괴
             field[mx][my] = "."
             break
         elif 0 <= mx < H and 0 <= my < W and field[mx][my] == "#":      # 강철 벽 만나면 부수지 못함
             break
-        elif 0 > mx or mx >= H or 0 > my or my >= H:
+        elif 0 > mx or mx >= H or 0 > my or my >= W:                    # 맵 밖으로 나가면 종료
             break
     
-    return field        # 포탄의 턴 한번 도출
+    return field        # 포탄이 발사된 뒤의 지도상태 반환
 
 
 def move(mobile, c):
-    x = mobile[1]
+    '''
+    mobile : 탱크의 현재 상태(바라보는 방향, i좌표, j좌표)
+    c : 입력받은 이동문자(UDLR)
+    '''
+    x = mobile[1]       # 현재 탱크의 위치 좌표
     y = mobile[2]
-    if c == 'U': 
-        if 0 <= x < H and 0 <= y < H and battle_field[x][y] not in '-*#':
-            battle_field[mobile[1]][mobile[2]] = '.'
-            mobile[1] = x
-            mobile[0] = '^'
-            battle_field[mobile[1]][mobile[2]] = mobile[0]
+    if c == 'U':        # UP
+        mobile[0] = '^'     # mobile의 바라보는 방향 설정
+        if 0 <= x-1 < H and 0 <= y < W and battle_field[x-1][y] not in '-*#':       # 가려는 방향이 맵 안이거나 장애물이 존재하지 않으면 해당 방향 1칸 직진
+            battle_field[x][y] = '.'                                                # 기존 위치는 평지로 변경
+            mobile[1] = x - 1                                                       # mobile(tank)의 좌표를 방향에 따라 변경해줌
+            battle_field[mobile[1]][mobile[2]] = mobile[0]                          # 전진한 위치에 탱크로 변경
+        else:                                                                       # 만약 움직일 수 없다면
+            battle_field[x][y] = mobile[0]                                          # 원래의 위치에서 방향만 바꿔줌
     elif c == 'D':
-        battle_field[mobile[1]][mobile[2]] = '.'
-        mobile[1] += mobile[1] + 1
         mobile[0] = 'v'
-        battle_field[mobile[1]][mobile[2]] = mobile[0]
+        if 0 <= x+1 < H and 0 <= y < W and battle_field[x+1][y] not in '-*#':
+            battle_field[x][y] = '.'
+            mobile[1] = x + 1
+            battle_field[mobile[1]][mobile[2]] = mobile[0]
+        else:
+            battle_field[x][y] = mobile[0]
     elif c == 'L':
-        battle_field[mobile[1]][mobile[2]] = '.'
-        mobile[2] = mobile[2] - 1
         mobile[0] = '<'
-        battle_field[mobile[1]][mobile[2]] = mobile[0]
+        if 0 <= x < H and 0 <= y-1 < W and battle_field[x][y-1] not in '-*#':
+            battle_field[x][y] = '.'
+            mobile[2] = y - 1
+            battle_field[mobile[1]][mobile[2]] = mobile[0]
+        else:
+            battle_field[x][y] = mobile[0]
     elif c == 'R':
-        battle_field[mobile[1]][mobile[2]] = '.'
-        mobile[2] = mobile[2] + 1
         mobile[0] = '>'
-        battle_field[mobile[1]][mobile[2]] = mobile[0]
+        if 0 <= x < H and 0 <= y+1 < W and battle_field[x][y+1] not in '-*#':
+            battle_field[x][y] = '.'
+            mobile[2] = y + 1
+            battle_field[mobile[1]][mobile[2]] = mobile[0]
+        else:
+            battle_field[x][y] = mobile[0]
     
     return mobile
 
@@ -98,10 +114,15 @@ for tc in range(1, T+1):
             if battle_field[i][j] in "><v^":
                 tank = [battle_field[i][j], i, j]
                 
-    for i in command:
+    for i in command:       # 커맨드 읽어가며 맵 상태 변환
         if i == 'S':
             battle_field = shoot(tank, battle_field)
         elif i in "UDLR":
-            tank = move(tank, i)
-            
-    print(battle_field)
+            tank = move(tank, i)    
+    
+    for i in range(H):      # 출력
+        if i == 0:
+            print(f'#{tc} {"".join(battle_field[i])}')
+        else:
+            print(f'{"".join(battle_field[i])}')
+    
