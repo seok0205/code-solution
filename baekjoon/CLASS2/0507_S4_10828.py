f066a7caa7c993c79d7ddbc65fe3e3d916d2baf3
seok0205 <9526yu@naver.com>
2025-05-07 20:47:19 +0900

    update 250507 S4 10828 스택

--- baekjoon/CLASS2/0507_S4_10828.py
+++ baekjoon/CLASS2/0507_S4_10828.py
@@ -10,40 +10,49 @@ empty : 스택에 비어있으면 1, 아니면 0
 top : 스택의 가장 위에든 정수 출력. 만약 스택에 들어있는 정수가 없는 경우 -1 출력.
 '''
 
-import sys
-sys.stdin = open('tc.txt', 'r')
+# import sys
+# sys.stdin = open('tc.txt', 'r')
 
 
-def push(num):
-    pass
+def push(num):          # 스택에 push(숫자 넣기)
+    stack.append(num)
 
 
-def pop():
-    pass
+def pop():              # pop : 가장 위 정수 빼고, 출력. 스택이 비어있다면 -1 출력
+    if stack:
+        print(stack.pop())
+    else:
+        print(-1)
 
 
-def size():
-    pass
+def size():             # size : 스택에 든 정수 개수 출력
+    print(len(stack))
 
 
-def empty():
-    pass
+def empty():            # empty : 스택에 비어있으면 1, 아니면 0
+    if stack:
+        print(0)
+    else:
+        print(1)
 
 
-def top():
-    pass
+def top():              # top : 스택의 가장 위에든 정수 출력. 만약 스택에 들어있는 정수가 없는 경우 -1 출력.
+    if stack:
+        print(stack[-1])
+    else:
+        print(-1)
 
 
 N = int(input())
-stack = list()
+stack = list()          # 명령에 따라 활용할 스택 생성
 
 for _ in range(N):
-    command = input()
+    command = input()       # 명령 입력 받기
 
-    if command[:4] == 'push':
-        com, num = map(input().split())
+    if command[:4] == 'push':       # push면 뒤에 숫자를 알아야함
+        com, num = map(str, command.split())        # 공백 단위로 둘로 나누고 num을 int화시킴
         num = int(num)
-        push(num)
+        push(num)                   # 정수화시킨 num을 푸시
     elif command == 'top':
         top()
     elif command == 'size':
